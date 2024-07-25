04bd4a857ea8a0e583fcfd8d9fb7cc598a82fb45
opuebla56 <pueblao@ferris.edu>
2024-07-24 23:17:15 -0400

    Created EWalletDB Class
    Create a separate .java file to access the DB called EWallet. (May need to be reviewed and revised.)

---
+++ DBTables.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Expenses (
+    ExpenseID varchar(255),
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
+
+CREATE TABLE Income (
+    IncomeID varchar(255),
+    IncomeAmount int,
+    Month int,
+);
+CREATE TABLE UserLogin {
+	UserID varchar(255),
+	UserEmail varchar(255),
+	UserPW varchar(255)
+);
