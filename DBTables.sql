21e7fb30a39943af59b596f9458530ec87ceef90
opuebla56 <pueblao@ferris.edu>
2024-07-30 19:11:16 -0400

    Update DBTables.sql
    Updated the expense table to reflect the correct attributes

--- DBTables.sql
+++ DBTables.sql
@@ -1,3 +1,20 @@
+
+
+
+CREATE TABLE EXPENSES (
+
+	ExpenseID varchar(255),
+	ExpenseAmount int,
+	Yearly int, 
+	CurrencyID varchar(255)
+	);
+	
+	CREATE TABLE Income (
+    IncomeID varchar(255),
+    IncomeAmount int,
+    Month int,
+);
+
 CREATE TABLE UserLogin (
 	UserID varchar(255),
 	UserEmail varchar(255),
