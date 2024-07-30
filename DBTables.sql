a0fc3765fd8d7e89801bee7efe5fc7c5aedc097f
opuebla56 <pueblao@ferris.edu>
2024-07-30 18:39:15 -0400

    Update DBTables.sql
    Created Tables for the Database

--- DBTables.sql
+++ DBTables.sql
@@ -1,17 +1,4 @@
-CREATE TABLE Expenses (
-    ExpenseID varchar(255),
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-);
-
-CREATE TABLE Income (
-    IncomeID varchar(255),
-    IncomeAmount int,
-    Month int,
-);
-CREATE TABLE UserLogin {
+CREATE TABLE UserLogin (
 	UserID varchar(255),
 	UserEmail varchar(255),
 	UserPW varchar(255)
